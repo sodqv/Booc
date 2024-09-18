d5ab81e3faa7129c72ec30f53730d0e4746b5fb3
Ebba Brage <ebbabrage@hotmail.com>
2024-09-18 10:00:10 +0200

    ..

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals.js';
-import Profile from './component/profile/Profile.js';
+import Profile from './component/profile/profile.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
