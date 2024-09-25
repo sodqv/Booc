c75885b80e51c0a02ffa86da7dd71728c6d3f913
Sara <saraodqvist@outlook.com>
2024-09-25 14:52:14 +0200

    Update meating.js

--- frontend/src/modelData/Feed/meating.js
+++ frontend/src/modelData/Feed/meating.js
@@ -13,7 +13,7 @@ export default function Meating() {
             borderRadius: '3px',
             boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)'
         }}>
-            <p style={{ fontSize: '25px'}}><b>18:30 - 19:30</b> Planing</p>
+            <p style={{ fontSize: '25px'}}><b>18:30 - 19:30</b> Planning</p>
             <div style={{  }}>
                 <p>Place:</p>
                 <p>Description:</p>
