6e599efa2d5652d3c125178d30f006c6cddba36f
Ebba Brage <ebbabrage@hotmail.com>
2024-10-03 15:13:22 +0200

    Added dates to table

--- frontend/src/modelData/Profile/table.js
+++ frontend/src/modelData/Profile/table.js
@@ -6,9 +6,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { alignProperty } from '@mui/material/styles/cssUtils';
+import dayjs from 'dayjs';
 
 function createData(
+  left,
   monday,
   tuesday,
   wednesday,
@@ -16,25 +17,26 @@ function createData(
   friday,
   saturday,
   sunday,
+  right
 ) {
-  return { monday, tuesday, wednesday, thursday, friday, saturday, sunday };
+  return {left, monday, tuesday, wednesday, thursday, friday, saturday, sunday, right };
 }
 
 const rows = [
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('', 'Meeting', '', '', '', '', ''),
-  createData('Meeting', '', '', 'Meeting', '', '', ''),
-  createData('Meeting', '', '', '', '', '', ''),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','', 'Meeting', '', '', '', '', ''),
+  createData('','Meeting', '', '', 'Meeting', '', '', ''),
+  createData('','Meeting', '', '', '', '', '', ''),
   createData('', '', '', '', 'Hairdresser', '', ''),
   createData('', 'Workout', '', 'Meeting', '', 'Party'),
   createData('', '', '', '', '', 'Party'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner'),
-  createData('Meeting', 'Dentist', '', '', '', 'Dinner')
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner'),
+  createData('','Meeting', 'Dentist', '', '', '', 'Dinner')
 ];
 const styleCellHead = {
   fontWeight: 'bold', 
@@ -45,28 +47,85 @@ const styleCell = {
   padding: '2px', 
   fontSize: '11px'
 }
+function nameOfDay(day) {
+  
+  switch(day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    default:
+      return "";
+  }
+}
 
 export default function BasicTable() {
+
+  const [selectedDate, setSelectedDate] = React.useState(dayjs());
+
+  // Start of week
+  const startOfWeek = selectedDate.startOf('week').add(1, 'day');
+
+  // Days in current week
+  const daysOfWeek = Array.from({ length: 7 }, (_, index) =>
+    startOfWeek.add(index, 'day').format('dddd D')
+  );
+
+  // Change week
+  const changeWeek = (amount) => {
+    setSelectedDate(selectedDate.add(amount, 'week'));
+  };
+
+  const todayCellStyle = {
+    ...styleCell,
+    backgroundColor: '#FFD700',  // Gold background for today
+    color: '#000',               // Text color to stand out
+  }
+  
+  const date = new Date();
+  const today = nameOfDay(date.getDay()) + " " + date.getDate();
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ tableLayout: 'fixed',minWidth: 400, bgcolor: '#f5f5f5', height: 'calc(100vh - 90px)' }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" sx={styleCellHead}>Monday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Tuesday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Wednesday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Thursday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Friday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Saturday</TableCell>
-            <TableCell align="center" sx={styleCellHead}>Sunday</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ tableLayout: 'fixed',minWidth: 400, bgcolor: '#f5f5f5', height: 'calc(100vh - 90px)' }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" sx={{...styleCellHead, width: '30px'}}>
+                <button variant="contained" onClick={() => changeWeek(-1)}>
+                  ←
+                </button>
+              </TableCell>
+
+              {daysOfWeek.map((day) => (
+                <TableCell 
+                  align="center" 
+                  sx={today == day ? todayCellStyle : styleCellHead} 
+                  key={day}>
+                  {day}
+                </TableCell>
+              ))}
+              
+              <TableCell align="center" sx={{...styleCellHead, width: '30px'}}>
+                <button variant="contained" onClick={() => changeWeek(1)} >
+                  →
+                </button>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody> {rows.map((row, index) => (
+            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell align="center" sx={ styleCell }>{row.left}</TableCell>
               <TableCell align="center" sx={ styleCell }>{row.monday}</TableCell>
               <TableCell align="center" sx={ styleCell }>{row.tuesday}</TableCell>
               <TableCell align="center" sx={ styleCell }>{row.wednesday}</TableCell>
@@ -74,10 +133,11 @@ export default function BasicTable() {
               <TableCell align="center" sx={ styleCell }>{row.friday}</TableCell>
               <TableCell align="center" sx={ styleCell }>{row.saturday}</TableCell>
               <TableCell align="center" sx={ styleCell }>{row.sunday}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <TableCell align="center" sx={ styleCell }>{row.right}</TableCell>
+            </TableRow>))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
