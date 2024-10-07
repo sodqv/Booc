2853a6e7117bd899491b3044b4ca9d5bcb3e9b2c
sandzan <sandra@carlsson4.se>
2024-10-07 23:24:19 +0200

    Added getEvents

--- frontend/src/component/feed/Feed.js
+++ frontend/src/component/feed/Feed.js
@@ -49,11 +49,11 @@ function Feed() {
 
             <div className="meatings" >
                 <h2>Monday</h2>
+                 <Meeting/>
+                {/*<Meeting/>
                 <Meeting/>
                 <Meeting/>
-                <Meeting/>
-                <Meeting/>
-                <Meeting/>
+                <Meeting/> */}
             </div>
 
             <div className="info">
--- frontend/src/modelData/Feed/meeting.js
+++ frontend/src/modelData/Feed/meeting.js
@@ -1,23 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getEvents } from '../event';
+import { useLoaderData } from 'react-router-dom';
+// css
 import './meeting.css';
 
 export default function Meeting() {
+    const user = useLoaderData();
 
+    // const meetings = getEvents(user);
+    // console.log("Full response:", meetings);
+
+    // const meetingName = `${meetings.title}`
     const [isOpen, setIsOpen] = useState(false);
+    const [meetings, setMeeting] = useState(false);
 
+    useEffect(() => {
+        const fetchEvent = async () => {
+            const event = await getEvents(user);
+            setMeeting(event);
+        }
+        fetchEvent();
+    }, [user]);
+
+    
     const toggleIsOpen = () => {setIsOpen(!isOpen)};
-
+    console.log(`Vad finns här:`, meetings);
     return (
         <div className="meating">
-            <p style={{ fontSize: '25px' }}><b>18:30 - 19:30</b> Planning</p>
+            <p style={{ fontSize: '25px' }}><b>18:30 - 19:30</b> {meetings[0].title} </p>
             <div className="collapsible" onClick={toggleIsOpen}>
+                <p>Creator:</p>
                 <p>Place:</p>
-                <p>Description:</p>
             </div>
             <div className="content" style={{ display: isOpen ? 'block' : 'none'}}>
+                <p>Description:</p>
                 <p>Participents:</p>
-                <p>Declined:</p>
-                <p>Creator:</p>
             </div>
         </div>
     );
